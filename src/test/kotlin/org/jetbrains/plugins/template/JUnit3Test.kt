e21555fa2395969b3bfb60cfe68e8ffa76ecf586
Arseniy Pendryak <arseniy.pendryak@jetbrains.com>
2021-09-24 14:30:55 +0300

    Pure JUnit 3 and JUnit 4 tests

---
+++ src/test/kotlin/org/jetbrains/plugins/template/JUnit3Test.kt
@@ -0,0 +1,10 @@
+package org.jetbrains.plugins.template
+
+import junit.framework.TestCase
+
+class JUnit3Test : TestCase() {
+
+    fun test() {
+        assertEquals("Hello!", "Hello")
+    }
+}
---
+++ src/test/kotlin/org/jetbrains/plugins/template/JUnit4Test.kt
@@ -0,0 +1,12 @@
+package org.jetbrains.plugins.template
+
+import org.junit.Assert
+import org.junit.Test
+
+class JUnit4Test {
+
+    @Test
+    fun hello() {
+        Assert.assertEquals("Hello!", "Hello")
+    }
+}
